d8b8bbe8f8bc0cd15d714d8ce42ff8755ef79824
rengadevi <vrengadevi@greatergoods.com>
2024-01-05 10:30:22 +0530

    Filtered valid links.

--- Qrcodescanner/app/src/main/java/com/example/qrcodescanner/shared/BarcodeAnalyser.kt
+++ Qrcodescanner/app/src/main/java/com/example/qrcodescanner/shared/BarcodeAnalyser.kt
@@ -1,27 +1,15 @@
 package com.example.qrcodescanner.shared
 
-import java.net.URL
 import android.annotation.SuppressLint
-import android.content.Context
-import android.net.Uri
-import android.provider.ContactsContract.CommonDataKinds.Website.URL
 import android.util.Log
-import android.util.Patterns
-import android.webkit.URLUtil
-import android.widget.Toast
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
-import java.net.HttpURLConnection
-import java.net.URISyntaxException
 import java.util.concurrent.TimeUnit
-import java.util.regex.Pattern
+
 
 @SuppressLint("UnsafeOptInUsageError")
 class BarCodeAnalyser(
@@ -64,16 +52,15 @@ class BarCodeAnalyser(
 
 
 fun parseLinkFromContent(scannedResult: MutableList<Barcode>): MutableList<String> {
-    Log.d("list00001",scannedResult.toString())
-    val urlPattern = ("((http|https)://)(www.)?" +
-     "[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]" +
-    "{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)").toPattern()
+
+    val urlPattern = ("((?:http|https)://)?(?:www\\.)?" +
+            "[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]" +
+            "{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)").toPattern()
 
     var urlList: MutableList<String>   = mutableListOf();
-scannedResult.forEach {
-    value ->
+      scannedResult.forEach {
+            value ->
     val matcher = value.rawValue?.let { urlPattern.matcher(it) }
-    Log.d("list00002",matcher.toString())
     if (matcher != null) {
         while (matcher.find()){
              var url = matcher.group()
@@ -84,5 +71,4 @@ scannedResult.forEach {
     return urlList
 }
     return mutableListOf("emptylist");
-
 }
